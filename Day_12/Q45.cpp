13396053ffc886ddfc3172f674f47bbe3ae12631
Ankur Yadav <itsankuryadav07@gmail.com>
2026-06-08 20:34:33 +0530

    Added Day 12 assignments

---
+++ Day_12/Q45.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int n) {
+    int original = n, rev = 0;
+
+    while (n > 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+
+    return original == rev;
+}
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (isPalindrome(num))
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
