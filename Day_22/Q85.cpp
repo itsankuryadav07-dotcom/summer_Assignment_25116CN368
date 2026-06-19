e28c2268e466db2a2180f71f8eaa356416817b7d
Ankur Yadav <itsankuryadav07@gmail.com>
2026-06-19 20:47:15 +0530

    Added Day 22 assignments

---
+++ Day_22/Q85.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    string rev = str;
+    reverse(rev.begin(), rev.end());
+
+    if(str == rev)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
